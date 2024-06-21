8b9f58989624785fa7e1467d60fa68dc7fef836e
BeckyLashua <beckylashua@gmail.com>
2024-06-21 09:58:54 -0500

    Initialized RetrievalForm component

---
+++ client/src/RetrievalForm.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Button from '@mui/material/Button';
+import axios from 'axios';
+
+const RetrievalForm = () => {
+  const handleClick = async () => {
+    try {
+      const response = await axios.get('http://localhost:3001/flight-data');
+      console.log(response.data);
+    } catch (error) {
+      console.error('Error making API call:', error);
+    }
+  };
+
+  return (
+    <Button variant="contained" color="primary" onClick={handleClick}>
+      Fetch Flight Data
+    </Button>
+  );
+};
+
+export default RetrievalForm;
