092fdb96986dca2ed68279c69387ee19042e2453
BeckyLashua <beckylashua@gmail.com>
2024-07-15 15:09:59 -0500

    Fixed dotenv loading issue

--- server/db.js
+++ server/db.js
@@ -1,19 +1,26 @@
-require('dotenv').config();
+const dotenv = require('dotenv');
 const { Pool } = require('pg');
 
-/*
+// Load environment variables from .env file
+const result = dotenv.config();
+if (result.error) {
+  throw result.error;
+}
+
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT
+});
+
+
 console.log('DB_USER:', process.env.DB_USER);
 console.log('DB_PASSWORD:', process.env.DB_PASSWORD);
 console.log('DB_DATABASE:', process.env.DB_DATABASE);
 console.log('DB_PORT:', process.env.DB_PORT);
-*/
 
-const pool = new Pool({
-  user: "rlashua",//process.env.DB_USER,
-  host: "localhost",//process.env.DB_HOST,
-  database: "flightapp",//process.env.DB_DATABASE,
-  password: 'Orphan718', //process.env.DB_PASSWORD,
-  port: 5432 //process.env.DB_PORT,
-});
+
 
 module.exports = pool;
